d2bd6d3f51b5c304e0dc29083e1fd6a7e684a06e
Michiel Meeuwissen <git@meeuw.org>
2005-09-02 12:35:09 +0000

    Renamed 'property' to (value)constraint. Because that is what they are. Proposing to move also validate-methods to the constraint impl. (will check first if that will not give problems in rmmci)

--- src/org/mmbase/bridge/remote/generator/MMCI.java
+++ src/org/mmbase/bridge/remote/generator/MMCI.java
@@ -126,7 +126,7 @@ public class MMCI {
         xmle.appendChild(ClassToXML.classToXML("org.mmbase.cache.CachePolicy",doc));
         // datatypes classes
         xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.DataType", doc));
-        xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.DataType$Property", doc));
+        xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.DataType$ValueConstraint", doc));
         xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.BigDataType",doc));
         xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.BinaryDataType",doc));
         xmle.appendChild(ClassToXML.classToXML("org.mmbase.datatypes.BooleanDataType",doc));
