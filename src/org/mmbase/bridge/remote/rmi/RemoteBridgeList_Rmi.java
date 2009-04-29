20da293c4e7bf152f339a3f92dd3f2d208c82fd3
Michiel Meeuwissen <git@meeuw.org>
2009-04-29 07:13:17 +0000

    spacing, javadoc

--- src/org/mmbase/bridge/remote/rmi/RemoteBridgeList_Rmi.java
+++ src/org/mmbase/bridge/remote/rmi/RemoteBridgeList_Rmi.java
@@ -19,37 +19,41 @@ import org.mmbase.bridge.remote.util.ObjectWrapper;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
+/**
+ * @javadoc Why is this implemented?
+ */
+
 public class RemoteBridgeList_Rmi<R, L> extends RemoteList_Rmi<R, L> implements RemoteBridgeList<R>, Unreferenced  {
-   private static Logger log = Logging.getLoggerInstance(RemoteBridgeList_Rmi.class);
+    private static Logger log = Logging.getLoggerInstance(RemoteBridgeList_Rmi.class);
 
-   public RemoteBridgeList_Rmi(BridgeList<L> originalObject) throws RemoteException{
-      super(originalObject);
-      log.debug("new RemoteBridgeList_Rmi");
-   }
-  public void setProperty(java.lang.Object arg0,java.lang.Object arg1) throws RemoteException {
-    getOriginalObject().setProperty(ObjectWrapper.rmiObjectToLocal(arg0), ObjectWrapper.rmiObjectToLocal(arg1));
-  }
+    public RemoteBridgeList_Rmi(BridgeList<L> originalObject) throws RemoteException{
+        super(originalObject);
+        log.debug("new RemoteBridgeList_Rmi");
+    }
+    public void setProperty(java.lang.Object arg0,java.lang.Object arg1) throws RemoteException {
+        getOriginalObject().setProperty(ObjectWrapper.rmiObjectToLocal(arg0), ObjectWrapper.rmiObjectToLocal(arg1));
+    }
 
-  public java.lang.Object getProperty(java.lang.Object arg0) throws RemoteException {
-    java.lang.Object retval = ObjectWrapper.localToRMIObject(getOriginalObject().getProperty(ObjectWrapper.rmiObjectToLocal(arg0)));
-    return retval;
-  }
+    public java.lang.Object getProperty(java.lang.Object arg0) throws RemoteException {
+        java.lang.Object retval = ObjectWrapper.localToRMIObject(getOriginalObject().getProperty(ObjectWrapper.rmiObjectToLocal(arg0)));
+        return retval;
+    }
 
-  public RemoteBridgeList<R> subList(int arg0,int arg1) throws RemoteException {
-    BridgeList<L> list = getOriginalObject().subList(arg0, arg1);
-    RemoteBridgeList<R> retval = (RemoteBridgeList<R>) ObjectWrapper.localToRMIObject(list);
-    return retval;
-  }
+    public RemoteBridgeList<R> subList(int arg0,int arg1) throws RemoteException {
+        BridgeList<L> list = getOriginalObject().subList(arg0, arg1);
+        RemoteBridgeList<R> retval = (RemoteBridgeList<R>) ObjectWrapper.localToRMIObject(list);
+        return retval;
+    }
 
-  public void sort(java.util.Comparator<? super R> arg0) {
-    throw new UnsupportedOperationException(" Method not supported in remote bridge ");
-  }
+    public void sort(java.util.Comparator<? super R> arg0) {
+        throw new UnsupportedOperationException(" Method not supported in remote bridge ");
+    }
 
-  public void sort() {
-    getOriginalObject().sort();
-  }
-  
-  public BridgeList<L> getOriginalObject() {
-      return (BridgeList<L>) super.getOriginalObject();
-  }
+    public void sort() {
+        getOriginalObject().sort();
+    }
+
+    public BridgeList<L> getOriginalObject() {
+        return (BridgeList<L>) super.getOriginalObject();
+    }
 }
