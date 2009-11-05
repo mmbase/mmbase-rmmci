4ca5cc3e7795dfe2402fc3eac3f9fe8e2572ccd7
Michiel Meeuwissen <git@meeuw.org>
2009-11-05 13:37:42 +0000

    processormodule is a bit silly

--- src/main/java/org/mmbase/module/RemoteMMCI.java
+++ src/main/java/org/mmbase/module/RemoteMMCI.java
@@ -31,7 +31,7 @@ import org.mmbase.util.logging.*;
  * @version $Id$
  * @since MMBase-1.5
  */
-public class RemoteMMCI extends ProcessorModule {
+public class RemoteMMCI extends Module {
 
     private Registry registry;
 
